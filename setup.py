923041deda8e9d772ced9004b59a68793236d148
Exthrempty <andrewsalt.e@gmail.com>
2020-08-31 16:41:26 +0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="qittle",
+    version="1.0.0",
+    author="exthrempty",
+    description="Indispensable solution for handling QIWI hooks",
+    url="https://github.com/exthrempty/qittle",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests~=2.24.0", "pydantic~=1.6.1", "starlette~=0.13.8",
+        "loguru~=0.5.1", "uvicorn~=0.11.8", "pyngrok~=4.1.10",
+    ]
+)
