3ab85145c34a795cb50940282af52b902506a655
Vladislav Kovalskyi <69077401+vladislavkovalskyi@users.noreply.github.com>
2020-09-05 13:20:55 +0300

    Update handler.py

--- qittle/hook/handler.py
+++ qittle/hook/handler.py
@@ -34,3 +34,4 @@ class HandlingProvider:
                 {**payment.dict(), **handler["payload"]} == payment.dict()
             ):
                 await handler["function"](payment)
+                break
